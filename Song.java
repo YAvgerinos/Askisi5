3f489b3b28643cf15fbda92a1bc366f7d37f28e6
YAvgerinos <y.avgerinos@yahoo.com>
2018-01-16 14:39:01 +0200

    askisi5

---
+++ Song.java
@@ -0,0 +1,58 @@
+/*Οικονομικό Πανεπιστήμιο Αθηνών
+  Μεταπτυχιακό στη Διοικητική Επιστήμη και Τεχνολογία
+  Full time - 2017-18
+  Συστήματα Λογισμικού: ’σκηση 5ης εβδομάδας
+
+  Αυγερινός Ιωάννης
+  MSTFT1702 */
+
+public class Song //Δημιουργία κλάσης Song
+{
+	//Δημιουργία ιδιωτικών μεταβλητών στιγμιοτύπου
+	private String title;
+	private Singer singer;
+	private String url;
+	//Μέθοδος δημιουργίας και αρχικοποίησης τιμών
+	public Song(String title, Singer singer, String url)
+	{
+		this.title = title;
+		this.singer = singer;
+		this.url = url;
+	}
+	//Μέθοδος getTitle
+	public String getTitle()
+	{
+		System.out.println(this.toString());
+		return title;
+	}
+	//Μέθοδος setTitle
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+	//Μέθοδος getSinger
+	public Singer getSinger()
+		{
+			return singer;
+	}
+	//Μέθοδος setSinger
+	public void setSinger(Singer singer)
+		{
+			this.singer = singer;
+	}
+	//Μέθοδος getUrl
+	public String getUrl()
+		{
+			return url;
+	}
+	//Μέθοδος setUrl
+	public void setUrl(String url)
+		{
+			this.url = url;
+	}
+	//Μέθοδος toString
+	public String toString()
+	{
+		return "Title: "+title+" -  Listen here: "+url;
+	}
+} //Τέλος κλάσης Song
